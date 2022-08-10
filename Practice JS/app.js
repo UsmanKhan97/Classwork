6cf79418965ab28e059fa2c0fa1166b332ba3576
uzzieh <61295953+uzzieh@users.noreply.github.com>
2022-08-10 16:04:03 -0400

    update

---
+++ Practice JS/app.js
@@ -0,0 +1,14 @@
+
+let letter = "a"
+        switch (letter)  {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                system.out.printIn(letter + "is vowel");
+                break;
+            default:
+                System.out.printIn(ch + "is consonant")
+        }
+    
