fb2834468c82bc3535ec41cb4494db3a38280d6d
uzzieh <61295953+uzzieh@users.noreply.github.com>
2022-08-11 15:55:58 -0400

    loop

---
+++ functions/app.js
@@ -0,0 +1,17 @@
+// // function declaration / definition
+// function sayHello(){
+//   console.log('Hello!')
+// }
+
+// function expression
+//const sayHi = function(){
+//    console.log('Hi!')
+// }
+
+// exercise one
+function computeArea(width,  hieght){
+    console.log("The area of  a rectangle with a width of " + width + "and a height of " + hieght + "is " + (width*hieght) + " square units." )
+}
+
+//exercise 2
+const
---
+++ functions/loop.js
@@ -0,0 +1,43 @@
+//exercise 1
+//for (let i = 10; i > 0; i -- ){
+//   console.log(i);
+//   if (i<1){
+//
+//   }
+//}
+
+//exercise 2
+//for (i=1; i <=10 ; i+=2){
+//    console.log(i)
+//}
+//for (i=2; i <=10; i+=2){
+//    console.log(i)
+//}
+//exercise 3
+//for (i=6; i<=60; i+=3){
+//   console.log(i);
+//   if (i>60){
+//    break;
+//}
+//}
+
+// exercise 4
+//let text = "";
+//for (let index=0; index <= 7; index++){
+//    console.log(text += "#");
+//}
+
+//let output = " " 
+//for(let i = 0; i <= 35; i++) { 
+//   if(i % 3 == 0) 
+//      output += i + " "
+//   } 
+//console.log(output)
+
+//let output = " " 
+//for(let i = 0; i <= 100; i++) { 
+//   if(i % 5 == 0) 
+//      output += i + " "
+//   } 
+//console.log(output)
+
